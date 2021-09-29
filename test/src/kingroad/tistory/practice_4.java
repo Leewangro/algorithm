8b984857f19019e62d42aa3efe67b8bfa626ee4c
Leewangro <dldhkdfh1@gmail.com>
2021-09-29 19:20:24 +0900

    행렬의 덧셈

--- test/src/kingroad/tistory/practice_4.java
+++ test/src/kingroad/tistory/practice_4.java
@@ -1,15 +1,38 @@
 package kingroad.tistory;
 
+import java.util.Arrays;
+
 public class practice_4 {
 
-	public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr1[0].length];
-        
-        for(int i=0; i<arr1.length; i++){
-            for(int j=0; j<arr1[i].length; j++){
-                answer[i][j] = arr1[i][j]+arr2[i][j];
-            }
-        }
-        return answer;
-    }
+	public static void main(String[] args) {
+
+		int x[][] = new int[5][10];
+		int y[][] = new int[3][5];
+
+		solution(x, y);
+	}
+
+	public static int[][] solution(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr1[0].length];
+
+		System.out.println(arr1.length);     // 배열의 사이즈
+		System.out.println(arr1[0].length);  // 배열의 첫 번째 행의 원소의 갯수 출력
+		/*
+		 * [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		 *  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		 *  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+		 *  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+		 *  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]  와 같은 구조로 이루어져 있음
+		 * 
+		 * */
+		System.out.println(Arrays.deepToString(answer));
+		
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr1[i].length; j++) {
+				answer[i][j] = arr1[i][j] + arr2[i][j];
+			}
+		}
+
+		return answer;
+	}
 }
