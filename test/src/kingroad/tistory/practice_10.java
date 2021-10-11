5c86ecd6ad94fd9f4aa1f59c8324f2ca0c72df45
Leewangro <dldhkdfh1@gmail.com>
2021-10-11 21:20:04 +0900

    짝수와 홀수

---
+++ test/src/kingroad/tistory/practice_10.java
@@ -0,0 +1,21 @@
+package kingroad.tistory;
+
+public class practice_10 {
+
+	public static void main(String[] args) {
+
+		System.out.println(solution(5));
+
+	}
+
+	public static String solution(int num) {
+        
+        if(num %2 == 0) {
+			return ("Even");
+		} else
+			return ("Odd");
+
+    }
+
+
+}
